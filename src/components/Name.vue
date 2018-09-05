ff6c6605d6938bfafa951abf5673b44c63085b49
Daniel Gordon <d_gordon1@icloud.com>
2018-09-05 13:40:02 +0200

    creating new components.

---
+++ src/components/Name.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="username">
+    <h1>{{ username }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Name",
+  props: {
+    username: String
+  }
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Name username="Daniel"></Name>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Name from "@/components/Name.vue";
+
+export default {
+  name: "about",
+  components: {
+    Name
+  }
+};
+</script>
