6a9834a97fd364da0fc7fce4522a0d69f0805ec5
Daniel Gordon <d_gordon1@icloud.com>
2018-09-06 14:36:13 +0200

    small edit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,11 +6,11 @@
 <script>
   export default {
     name: 'contact',
-    mounted() {
-      console.log('mounted contact component')
-    this.$root.$on('messageChanged', (dataMessage) => {
-      alert("Hello from the main Contact component" + " " + dataMessage);
-    })
-  }
+      mounted() {
+        console.log('mounted contact component')
+      this.$root.$on('messageChanged', (dataMessage) => {
+        alert("Hello from the main Contact component" + " " + dataMessage);
+      })
+    }
   }
 </script>
